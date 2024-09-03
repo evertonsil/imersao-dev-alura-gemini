0458b0df1326b70e24bf1a21cce0df339824346f
evertonsil <everton.marques@uni9.edu.br>
2024-09-03 11:39:07 -0300

    adjusted header and theme toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
 //alternando entre tema claro e escuro
 document.getElementById('theme-toggle').addEventListener('click', function () {
-    var root = document.documentElement;
-    var themeIconClaro = document.getElementById('theme-icon-claro');
-    var themeIconEscuro = document.getElementById('theme-icon-escuro');
+    let root = document.documentElement;
+    let themeIconClaro = document.getElementById('theme-icon-claro');
+    let themeIconEscuro = document.getElementById('theme-icon-escuro');
+    let logo = document.getElementById('porscheLogo');
+    let header = document.querySelector('header');
 
     if (root.getAttribute('data-bs-theme') === 'dark') {
         root.setAttribute('data-bs-theme', 'light');
         themeIconClaro.style.display = 'none';
         themeIconEscuro.style.display = 'inline';
+        logo.style.fill = '#212529';
+        header.style.backgroundColor = '#00000017';
     } else {
         root.setAttribute('data-bs-theme', 'dark');
         themeIconClaro.style.display = 'inline';
         themeIconEscuro.style.display = 'none';
+        logo.style.fill = '#ffffff';
+        header.style.backgroundColor = '#ffffff17';
     }
 });
